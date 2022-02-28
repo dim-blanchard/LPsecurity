1f053b031c6489014a9cc422725f3ec0c2f526a2
BuildTools <unconfigured@null.spigotmc.org>
2022-03-01 00:11:31 +0100

    first commit

---
+++ src/main/java/fr/loirelique/lpsecurity/Main.java
@@ -0,0 +1,43 @@
+package fr.loirelique.lpsecurity;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+/**
+ * Information sur la class!LPSECURITY
+ *
+ */
+
+
+
+public class Main extends JavaPlugin implements Listener{
+    
+
+    @Override
+    public void onEnable(){
+        // Actions à effectuer au démarrage du plugin, c'est-à-dire :
+        //   - Au démarrage du serveur
+        //   - Après un /reload
+
+        System.out.println("Plugin pour la sécurité Erizia activer.");
+    }
+
+
+    @Override
+    public void onDisable(){
+        // Actions à effectuer à la désactivation du plugin
+        //   - A l'extinction du serveur
+        //   - Pendant un /reload
+
+        System.out.println("Plugin pour la sécurité Erizia désactiver.");
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
