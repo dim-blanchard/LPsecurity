61d9d81272a42cc2656dad3a314e9e1f53405b8a
BuildTools <unconfigured@null.spigotmc.org>
2022-04-15 14:22:10 +0200

    TEST SQL + SAISIE DATE

--- src/main/java/fr/loirelique/lpsecurity/MyClassTest.java
+++ src/main/java/fr/loirelique/lpsecurity/MyClassTest.java
@@ -6,6 +6,7 @@ import java.text.Normalizer;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class MyClassTest {
@@ -22,100 +23,119 @@ public class MyClassTest {
         }
     }
 
-    public static String sansAccent(String s) 
-    {
-   
-          String strTemp = Normalizer.normalize(s, Normalizer.Form.NFD);
-          Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
-          return pattern.matcher(strTemp).replaceAll("");
+    public static String sansAccent(String s) {
+
+        String strTemp = Normalizer.normalize(s, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(strTemp).replaceAll("");
     }
 
     public static void main(String args[]) {
 
-        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yy/MM/dd HH:mm:ss");
-        System.out.println("yy/MM/dd HH:mm:ss-> "+dtf2.format(ZonedDateTime.now(ZoneId.of("Europe/Paris"))));
-
-        /* SELECT historique_sanctions->'$.motifs_sanctions.ban' FROM pf8kr9g9players ; */
-        /* UPDATE pf8kr9g9players SET historique_sanctions = JSON_SET(historique_sanctions, '$.motifs_sanction.ban',2); */
-
-
-
-        /* {"motifs_sanction": {"ban": "","tempban": "","unban": "","mute": "","tempmute": "","unmute": "","warn":"","kick": ""},"temp_sanction": {"ban": "","mute": ""}} */
-
-
-
-/*         INSERT INTO pf8kr9g9players (historique_sanctions) VALUES ('{"motifs_sanction": {
-            "ban": "",
-            "tempban": "",
-            "unban": "",
-            "mute": "",
-            "tempmute": "",
-            "unmute": "",
-            "warn":"",
-            "kick": ""
-          },
-        
-          "temp_sanction": {
-            "ban": "",
-            "mute": ""
-          },}'); */
-
-
- /* '{"motif_ban": "null","motif_tempban": "null","motif_unban": "null","motif_mute": "null","motif_tempmute": "null","motif_unmute": "null","motif_warn": "null","motif_kick": "null","temp_ban": "null","temp_mute": "null"}' */
-
-
- /*        
-        String[] argss = new String[3];
-
-        argss[0]="J'ai";
-        argss[1]="peut être fait";
-        argss[2]="une érreur.";
-
-
-            // Chaîne avec accent
-    String test="Marché public, école, j'ai programmé, chaîne de caractère";
- 
-    // Effacer les accents de la chaîne de caractère 'test'
-    String str_sans_accent=sansAccent(test);
- 
-    //Afficher le résultat
-    System.out.println(str_sans_accent);    
-
-
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < argss.length; i++) {
-            String ar = sansAccent(argss[i]);
-            builder.append(ar).append(" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Veuillez saisir une date(yy/MM/dd HH:mm:ss):");
+            String str = sc.nextLine();
+            System.out.println("Vous avez saisi : " + str);
         }
-        String msg = builder.toString();
-        System.out.println(msg); */
+        /*
+         * DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yy/MM/dd HH:mm:ss");
+         * System.out.println("yy/MM/dd HH:mm:ss-> "+dtf2.format(ZonedDateTime.now(
+         * ZoneId.of("Europe/Paris"))));
+         */
 
+        /*
+         * SELECT historique_sanctions->'$.motifs_sanctions.ban' FROM pf8kr9g9players ;
+         */
+        /*
+         * UPDATE pf8kr9g9players SET historique_sanctions =
+         * JSON_SET(historique_sanctions, '$.motifs_sanction.ban',2);
+         */
 
-/*           // start stopwatch
-          //for(int i =0 ; i < 10 ; i++){
-          long startTime = System.nanoTime();
-          // Here is the code to measure
-          getIpOfPlayerBeforeLogin("1","qsfseeeeeqzgzergzgrgrggggggzrgzrgzrgzergzrg");
-          getIpOfPlayerBeforeLogin("1","zgrzrgzgrzgggggrgrgrgrgrgqgrgqzrgtergqegzgg");
-          getIpOfPlayerBeforeLogin("1","zrgzzgqqqqegeqggbzrbqwiukkkkkkkkkkkukgukget");
-          getIpOfPlayerBeforeLogin("1","gergesbserbgsh'rjtgshey'j( y,cjc,cjc;j;,,,,");
-          getIpOfPlayerBeforeLogin("1","jgdergwdrgwdrgzer gb'tzbesgnjetgdxrnhnxhtnn");
-          getIpOfPlayerBeforeLogin("1","xnyte(kxtj,kxxynjhxhbrxbgxrxnjrwrthfhjytjyj");
-          
-          System.out.println(listArrays.get("1"));
-          System.out.println(listArrays.get("1").size());
-          
-          listArrays.get("1").remove("zrgzzgqqqqegeqggbzrbqwiukkkkkkkkkkkukgukget");
-          
-          System.out.println(listArrays.get("1").size());
-          
-          System.out.println(listArrays.get("1"));
-          // stop stopwatch
-          long endTime = System.nanoTime();
-          
-          System.out.println("Test de vitesse : " + (endTime -
-          startTime)*Math.pow(10,-6) + " ms"); */
-        
+        /*
+         * {"motifs_sanction": {"ban": "","tempban": "","unban": "","mute":
+         * "","tempmute": "","unmute": "","warn":"","kick": ""},"temp_sanction": {"ban":
+         * "","mute": ""}}
+         */
+
+        /*
+         * INSERT INTO pf8kr9g9players (historique_sanctions) VALUES
+         * ('{"motifs_sanction": {
+         * "ban": "",
+         * "tempban": "",
+         * "unban": "",
+         * "mute": "",
+         * "tempmute": "",
+         * "unmute": "",
+         * "warn":"",
+         * "kick": ""
+         * },
+         * 
+         * "temp_sanction": {
+         * "ban": "",
+         * "mute": ""
+         * },}');
+         */
+
+        /*
+         * '{"motif_ban": "null","motif_tempban": "null","motif_unban":
+         * "null","motif_mute": "null","motif_tempmute": "null","motif_unmute":
+         * "null","motif_warn": "null","motif_kick": "null","temp_ban":
+         * "null","temp_mute": "null"}'
+         */
+
+        /*
+         * String[] argss = new String[3];
+         * 
+         * argss[0]="J'ai";
+         * argss[1]="peut être fait";
+         * argss[2]="une érreur.";
+         * 
+         * 
+         * // Chaîne avec accent
+         * String test="Marché public, école, j'ai programmé, chaîne de caractère";
+         * 
+         * // Effacer les accents de la chaîne de caractère 'test'
+         * String str_sans_accent=sansAccent(test);
+         * 
+         * //Afficher le résultat
+         * System.out.println(str_sans_accent);
+         * 
+         * 
+         * StringBuilder builder = new StringBuilder();
+         * for (int i = 0; i < argss.length; i++) {
+         * String ar = sansAccent(argss[i]);
+         * builder.append(ar).append(" ");
+         * }
+         * String msg = builder.toString();
+         * System.out.println(msg);
+         */
+
+        /*
+         * // start stopwatch
+         * //for(int i =0 ; i < 10 ; i++){
+         * long startTime = System.nanoTime();
+         * // Here is the code to measure
+         * getIpOfPlayerBeforeLogin("1","qsfseeeeeqzgzergzgrgrggggggzrgzrgzrgzergzrg");
+         * getIpOfPlayerBeforeLogin("1","zgrzrgzgrzgggggrgrgrgrgrgqgrgqzrgtergqegzgg");
+         * getIpOfPlayerBeforeLogin("1","zrgzzgqqqqegeqggbzrbqwiukkkkkkkkkkkukgukget");
+         * getIpOfPlayerBeforeLogin("1","gergesbserbgsh'rjtgshey'j( y,cjc,cjc;j;,,,,");
+         * getIpOfPlayerBeforeLogin("1","jgdergwdrgwdrgzer gb'tzbesgnjetgdxrnhnxhtnn");
+         * getIpOfPlayerBeforeLogin("1","xnyte(kxtj,kxxynjhxhbrxbgxrxnjrwrthfhjytjyj");
+         * 
+         * System.out.println(listArrays.get("1"));
+         * System.out.println(listArrays.get("1").size());
+         * 
+         * listArrays.get("1").remove("zrgzzgqqqqegeqggbzrbqwiukkkkkkkkkkkukgukget");
+         * 
+         * System.out.println(listArrays.get("1").size());
+         * 
+         * System.out.println(listArrays.get("1"));
+         * // stop stopwatch
+         * long endTime = System.nanoTime();
+         * 
+         * System.out.println("Test de vitesse : " + (endTime -
+         * startTime)*Math.pow(10,-6) + " ms");
+         */
 
         // }
 
@@ -143,10 +163,12 @@ public class MyClassTest {
          * }
          */
 
-       /*  String str = "Bonjour vous         allez                   bien";
-        System.out.println("Mot avec des espaces: " + str);
-        str = str.replaceAll("\\s", "");
-        System.out.println("Mot sans des espaces: " + str); */
+        /*
+         * String str = "Bonjour vous         allez                   bien";
+         * System.out.println("Mot avec des espaces: " + str);
+         * str = str.replaceAll("\\s", "");
+         * System.out.println("Mot sans des espaces: " + str);
+         */
 
     }
 }
