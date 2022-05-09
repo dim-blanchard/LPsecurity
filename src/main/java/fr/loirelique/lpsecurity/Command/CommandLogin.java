715a3c45bf9df0dfb186328ce8c2cbd1ad90a01b
BuildTools <unconfigured@null.spigotmc.org>
2022-05-09 16:08:57 +0200

    Mise à jour command

--- src/main/java/fr/loirelique/lpsecurity/Command/CommandLogin.java
+++ src/main/java/fr/loirelique/lpsecurity/Command/CommandLogin.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.HashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
